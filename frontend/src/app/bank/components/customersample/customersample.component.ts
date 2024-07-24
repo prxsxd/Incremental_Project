2291b2e74cb351042596d071e945458500fd97d3
username <wecp-dummy@gmail.com>
2024-07-24 13:05:54 +0000

    Day 16 and 17 task

--- frontend/src/app/bank/components/customersample/customersample.component.ts
+++ frontend/src/app/bank/components/customersample/customersample.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Customer } from '../../types/Customer';
+import { CustomerTS } from '../../types/tstypes/Customerts';
 import { of } from 'rxjs';
-
+ 
 @Component({
   selector: 'app-customersample',
   standalone: true,
   imports: [],
   templateUrl: './customersample.component.html',
-  styleUrl: './customersample.component.css'
+  styleUrls: ['./customersample.component.css']
 })
 export class CustomersampleComponent {
  
+  customer : CustomerTS = new CustomerTS("Abhishek","abhishek@gmail.com","Abhishek1061", "abcdefg", "Developer", "2");
+ 
 }
-
--- frontend/src/app/bank/types/Transaction.ts
+++ frontend/src/app/bank/types/Transaction.ts
@@ -1,28 +1,28 @@
 
-import { Account } from './Account';
+// import { Account } from './Account';
 
-export class Transaction {
-    transactionId?: number;
-    accountId: string;
-  amount: number;
-  transactionDate: Date;
-   transactionType:string;
+// export class Transaction {
+//     transactionId?: number;
+//     accountId: string;
+//   amount: number;
+//   transactionDate: Date;
+//    transactionType:string;
 
-    constructor(accountId: string, amount: number, transactionDate: Date, transactionId?:number) {
-        this.transactionId = transactionId;
-        this.accountId = accountId;
-        this.amount = amount;
-        this.transactionDate = transactionDate;
-    }
+//     constructor(accountId: string, amount: number, transactionDate: Date, transactionId?:number) {
+//         this.transactionId = transactionId;
+//         this.accountId = accountId;
+//         this.amount = amount;
+//         this.transactionDate = transactionDate;
+//     }
 
-    displayInfo() {
-        console.log(`Transaction ID: ${this.transactionId}`);
-        console.log(`Account ID: ${this.accountId}`);
-        console.log(`Amount: ${this.amount.toFixed(2)}`);
-        console.log(`Transaction Date: ${this.transactionDate}`);
-        console.log("------");
-    }
-}
+//     displayInfo() {
+//         console.log(`Transaction ID: ${this.transactionId}`);
+//         console.log(`Account ID: ${this.accountId}`);
+//         console.log(`Amount: ${this.amount.toFixed(2)}`);
+//         console.log(`Transaction Date: ${this.transactionDate}`);
+//         console.log("------");
+//     }
+// }
 
-const transaction = new Transaction("1", 50.00, new Date(),1);
-transaction.displayInfo();
+// const transaction = new Transaction("1", 50.00, new Date(),1);
+// transaction.displayInfo();
